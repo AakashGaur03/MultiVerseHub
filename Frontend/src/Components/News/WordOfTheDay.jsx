96b6b6f34a6d249525e6f552da3854ead002e713
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2024-04-24 16:56:28 +0530

    Integrating Word Of The Day

--- Frontend/src/Components/News/WordOfTheDay.jsx
+++ Frontend/src/Components/News/WordOfTheDay.jsx
@@ -1,12 +1,17 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const WordOfTheDay = () => {
+    const [wordOfTheDayData,setWordOfTheDayData]=  useState(
+        JSON.parse(localStorage.getItem("wordOfTheDay")) || null
+    );
 //   useEffect(() => {
     const handleClicl = async () => {
+        const currentDate = new Date().toDateString();
+        const storedDate = localStorage.getItem("wordOfTheDayDate") || null;
+
       const options = {
         method: "GET",
-        // url: "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf",
         url: "https://words-api5.p.rapidapi.com/api/v1/dict/word-today",
         headers: {
           "X-RapidAPI-Key":
@@ -16,16 +21,35 @@ const WordOfTheDay = () => {
       };
 
       try {
-        const response = await axios.request(options);
-        console.log(response.data);
+        console.log(storedDate)
+        console.log(currentDate)
+        if ((!wordOfTheDayData || !wordOfTheDayData.word) && storedDate != currentDate) {
+            const response = await axios.request(options);
+            const wordData = response.data.data;
+            console.log(wordData);
+            setWordOfTheDayData(wordData);
+            localStorage.setItem("wordOfTheDay", JSON.stringify(wordData));
+            localStorage.setItem("wordOfTheDayDate", currentDate);
+
+            
+        }
       } catch (error) {
         console.error(error);
       }
     };
+    useEffect(()=>{
+        console.log('useEffect triggered');
+        handleClicl()
+    },[])
 //   });
   return (
     <>
       <button onClick={handleClicl}>CLICK</button>
+      <div>
+      <h2>Word of the Day</h2>
+                    <p>Word: {wordOfTheDayData.word}</p>
+                    <p>Meaning: {wordOfTheDayData.meaning}</p>
+      </div>
     </>
   );
 };
