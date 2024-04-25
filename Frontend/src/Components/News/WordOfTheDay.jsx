45b46d8a09c25bfc9c4ea490c8379bb6ccd94237
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2024-04-25 22:10:31 +0530

    Updations Word Of the Day

--- Frontend/src/Components/News/WordOfTheDay.jsx
+++ Frontend/src/Components/News/WordOfTheDay.jsx
@@ -2,53 +2,53 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const WordOfTheDay = () => {
-    const [wordOfTheDayData,setWordOfTheDayData]=  useState(
-        JSON.parse(localStorage.getItem("wordOfTheDay")) || null
-    );
-//   useEffect(() => {
-    const handleClicl = async () => {
-        const currentDate = new Date().toDateString();
-        const storedDate = localStorage.getItem("wordOfTheDayDate") || null;
+  const [wordOfTheDayData, setWordOfTheDayData] = useState(
+    JSON.parse(localStorage.getItem("wordOfTheDay")) || null
+  );
+  //   useEffect(() => {
+  const handleClicl = async () => {
+    const currentDate = new Date().toDateString();
+    const storedDate = localStorage.getItem("wordOfTheDayDate") || null;
 
-      const options = {
-        method: "GET",
-        url: "https://words-api5.p.rapidapi.com/api/v1/dict/word-today",
-        headers: {
-          "X-RapidAPI-Key":
-            "bb3721361emshddcfed580ee75dap16315bjsn1b92129b04d2",
-          "X-RapidAPI-Host": "words-api5.p.rapidapi.com",
-        },
-      };
-
-      try {
-        console.log(storedDate)
-        console.log(currentDate)
-        if ((!wordOfTheDayData || !wordOfTheDayData.word) && storedDate != currentDate) {
-            const response = await axios.request(options);
-            const wordData = response.data.data;
-            console.log(wordData);
-            setWordOfTheDayData(wordData);
-            localStorage.setItem("wordOfTheDay", JSON.stringify(wordData));
-            localStorage.setItem("wordOfTheDayDate", currentDate);
-
-            
-        }
-      } catch (error) {
-        console.error(error);
-      }
+    const options = {
+      method: "GET",
+      url: "https://words-api5.p.rapidapi.com/api/v1/dict/word-today",
+      headers: {
+        "X-RapidAPI-Key": "bb3721361emshddcfed580ee75dap16315bjsn1b92129b04d2",
+        "X-RapidAPI-Host": "words-api5.p.rapidapi.com",
+      },
     };
-    useEffect(()=>{
-        console.log('useEffect triggered');
-        handleClicl()
-    },[])
-//   });
+
+    try {
+      if (
+        !wordOfTheDayData ||
+        !wordOfTheDayData.word ||
+        storedDate != currentDate
+      ) {
+        const response = await axios.request(options);
+        const wordData = response.data.data;
+        setWordOfTheDayData(wordData);
+        localStorage.setItem("wordOfTheDay", JSON.stringify(wordData));
+        localStorage.setItem("wordOfTheDayDate", currentDate);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    handleClicl();
+  }, []);
+  //   });
   return (
     <>
-      <button onClick={handleClicl}>CLICK</button>
-      <div>
-      <h2>Word of the Day</h2>
-                    <p>Word: {wordOfTheDayData.word}</p>
-                    <p>Meaning: {wordOfTheDayData.meaning}</p>
+      <div className="border-blue-950 text-center flex flex-col justify-evenly h-52 m-8 border-2 wordOfTheDay border-solid p-2">
+        <div>
+          <h2>Word of the Day</h2>
+        </div>
+        <div>
+          <p className="text-left">Word : {wordOfTheDayData.word}</p>
+          <p className="text-left">Meaning : {wordOfTheDayData.meaning}</p>
+        </div>
       </div>
     </>
   );
