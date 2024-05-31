0fe01bc019d09e2eb41fb5c8b5fffbaa8d5e9cf3
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2024-05-31 23:31:09 +0530

    Adding Rankings & Removed Irrelevant Code from Cricket Comp

--- Frontend/src/Components/Cricket/Cricket.jsx
+++ Frontend/src/Components/Cricket/Cricket.jsx
@@ -24,140 +24,11 @@ const Cricket = ({
   // const [cricketData, setCricketData] = useState([]);
   const [pointstable, setpointstable] = useState({ id: null, data: [] });
 
-  // useEffect(() => {
-  //   dispatch(getCricket()).then((response) => {
-  //     const typeMatches = response.data.responseData.typeMatches;
-  //     console.log(response, "responseOFCRi");
-
-  //     let LeaguesMatches = typeMatches.find(
-  //       (match) => match.matchType == "League"
-  //     );
-  //     let InterMatches = typeMatches.find(
-  //       (match) => match.matchType == "International"
-  //     );
-  //     let WomenMatches = typeMatches.find(
-  //       (match) => match.matchType == "Women"
-  //     );
-
-  //     let IPLMatches = LeaguesMatches?.seriesMatches
-  //       .find((matchseries) =>
-  //         matchseries.seriesAdWrapper.seriesName.includes(
-  //           "Indian Premier League"
-  //         )
-  //       )
-  //       .seriesAdWrapper.matches?.slice(0, 3);
-
-  //     let IntlMatches = InterMatches.seriesMatches
-  //       .filter((match) => match.seriesAdWrapper)
-  //       .slice(0, 2); // It slices number of series to 2
-
-  //     let WomMatches = WomenMatches.seriesMatches
-  //       .filter((match) => match.seriesAdWrapper)
-  //       .slice(0, 2); // It slices number of series to 2
-
-  //     // console.log(IntlMatches, "IntlMatches");
-
-  //     let newCricketData = [];
-
-  //     // Adding IPL matches to newCricketData
-  //     if (Array.isArray(IPLMatches)) {
-  //       newCricketData.push(...IPLMatches.slice(0, 3));
-  //     }
-
-  //     // Adding International matches to newCricketData
-  //     IntlMatches.forEach((match) => {
-  //       if (Array.isArray(match.seriesAdWrapper.matches)) {
-  //         newCricketData.push(...match.seriesAdWrapper.matches.slice(0, 2)); // It slices mathces in series to 2
-  //       }
-  //     });
-  //     WomMatches.forEach((match) => {
-  //       if (Array.isArray(match.seriesAdWrapper.matches)) {
-  //         newCricketData.push(...match.seriesAdWrapper.matches.slice(0, 2)); // It slices mathces in series to 2
-  //       }
-  //     });
-
-  //     // Update the state once with all the data
-  //     setCricketData(newCricketData);
-  //   });
-  // }, []);
-
-  //   const getCricketImage = async (imageId) => {
-  //     try {
-  //         const response = await getCricketImageAPIFunc(imageId);
-  //         console.log(response, "response");
-
-  //         // Check if response data is a Blob
-  //         if (!(response instanceof Blob)) {
-  //             throw new Error('Response data is not a Blob.');
-  //         }
-
-  //         const reader = new FileReader(); // Create a new FileReader object
-
-  //         reader.readAsDataURL(response); // Read the blob data as a data URL
-
-  //         reader.onload = () => {
-  //             const imageSrc = reader.result; // Get the base64-encoded data URL
-  //             console.log(imageSrc, "imageSrc");
-  //             setImageSrc(imageSrc); // Set the image source for rendering
-  //         };
-  //     } catch (error) {
-  //         console.error("Error fetching cricket image:", error);
-  //         return null;
-  //     }
-  // };
-
-  // const [imageSrc, setImageSrc] = useState("");
-
-  // useEffect(() => {
-  //   const fetchImage = async () => {
-  //     const options = {
-  //       method: "GET",
-  //       url: "https://cricbuzz-cricket.p.rapidapi.com/img/v1/i1/c231889/i.jpg",
-  //       headers: {
-  //         "X-RapidAPI-Key":
-  //           "",
-  //         "X-RapidAPI-Host": "cricbuzz-cricket.p.rapidapi.com",
-  //       },
-  //       responseType: "blob", // Set the response type to blob
-  //     };
-
-  //     try {
-  //       const response = await axios.request(options);
-  //       console.log(response, "respmse22");
-  //       const reader = new FileReader(); // Create a new FileReader object
-  //       console.log(reader, "reader");
-
-  //       reader.readAsDataURL(response.data); // Read the blob data as a data URL
-
-  //       reader.onload = () => {
-  //         const imageSrc = reader.result; // Get the base64-encoded data URL
-  //         console.log(reader.result, "resdrresyl");
-  //         setImageSrc(imageSrc); // Set the image source for rendering
-  //       };
-  //     } catch (error) {
-  //       console.error("Error fetching cricket image:", error);
-  //     }
-  //   };
-
-  //   fetchImage();
-  // }, []);
   const getPointsTable = async (id) => {
-    const options = {
-      method: "GET",
-      url: `https://cricbuzz-cricket.p.rapidapi.com/stats/v1/series/${id}/points-table`,
-      headers: {
-        "x-rapidapi-key": "22d26b47d8msh803eeb4fcc0d938p12274fjsna785b41ba11b",
-        "x-rapidapi-host": "cricbuzz-cricket.p.rapidapi.com",
-        "Content-Type": "application/json",
-      },
-    };
-
     try {
       const response = await getCricketPointsTableAPIFunc(id);
       console.log(response, "1");
-      // console.log(response.pointsTable, "3");
       setQuery("");
-      // console.log(response.pointsTable[0].pointsTableInfo, "pointsTableInfo");
       let datatoStrore = {};
       if (response) {
         datatoStrore = {
@@ -171,7 +42,6 @@ const Cricket = ({
         };
       }
       setpointstable(datatoStrore);
-      // console.log(pointstable.data, "pointstable");
 
       navigate(`${id}/pointsTable`, {
         state: { pointsTableData: datatoStrore },
@@ -180,6 +50,30 @@ const Cricket = ({
       console.error(error);
     }
   };
+
+  const getRanking = async() =>{
+      // console.log(pointstable.data, "pointstable");
+
+      const options = {
+        method: 'GET',
+        url: 'https://cricbuzz-cricket.p.rapidapi.com/stats/v1/rankings/batsmen',
+        params: {
+          formatType: 'odi',
+          isWomen: '1'
+        },
+        headers: {
+          'x-rapidapi-key': '94d5879a35msh63d070accbd04e3p13e33ejsnb6869afe3816',
+          'x-rapidapi-host': 'cricbuzz-cricket.p.rapidapi.com'
+        }
+      };
+      
+      try {
+        const response = await axios.request(options);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+  }
 
   return (
     <>
@@ -235,10 +129,6 @@ const Cricket = ({
               <div></div>
               <div>{data.matchInfo?.status}</div>
               <NavLink
-                // to={{
-                //   pathname: `${data.matchInfo.seriesId}/pointsTable`,
-                //   state: { pointsTableData: pointstable },
-                // }}
                 className="cursor-pointer"
                 onClick={() => getPointsTable(data.matchInfo.seriesId)}
               >
@@ -246,6 +136,8 @@ const Cricket = ({
               </NavLink>
             </div>
           ))}
+
+          <button onClick={getRanking}>Get Ranks</button>
       </div>
     </>
   );
