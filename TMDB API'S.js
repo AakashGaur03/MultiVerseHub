84590f514c5d515be3082b06be6316ebc33c74d4
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2024-07-02 22:11:34 +0530

    TMDB API's

---
+++ TMDB API'S.js
@@ -0,0 +1,134 @@
+// Movies
+
+// const fetch = require('node-fetch');
+
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer (AUTHORIZATION HEADER)'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
+
+
+
+// GENERES MOVIES
+// const fetch = require('node-fetch');
+
+// const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer (AUTHORIZATION HEADER)'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
+
+
+
+
+
+// INFO ABOUT SINGLE MOVIE
+// https://api.themoviedb.org/3/movie/786892?api_key=27466545a65659fcc5cb0e1571b23956
+
+// CREDITS
+// https://api.themoviedb.org/3/movie/786892/credits?api_key=27466545a65659fcc5cb0e1571b23956
+
+// REVIEWS
+// https://api.themoviedb.org/3/movie/786892/reviews?api_key=27466545a65659fcc5cb0e1571b23956
+
+
+
+
+// GET API
+// https://api.themoviedb.org/3/movie/top_rated?api_key=(API_KEY)
+
+// POPULAR
+// https://api.themoviedb.org/3/movie/popular?api_key=(API_KEY)
+
+// NOW PLAYING
+// https://api.themoviedb.org/3/movie/now_playing?api_key=(API_KEY)
+
+// MOVIE SEARCHING
+// https://api.themoviedb.org/3/search/movie?api_key=(API_KEY)&query=H
+
+
+
+
+
+
+// TV
+
+// const fetch = require('node-fetch');
+
+// const url = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer (AUTHORIZATION HEADER)'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
+
+
+
+// GENERES TV
+
+// const fetch = require('node-fetch');
+
+// const url = 'https://api.themoviedb.org/3/genre/tv/list?language=en';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer (AUTHORIZATION HEADER)'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error('error:' + err));
+
+
+
+
+// INFO ABOUT SINGLE TV SHOW
+// https://api.themoviedb.org/3/tv/20169?api_key=27466545a65659fcc5cb0e1571b23956
+
+// CREDITS
+// https://api.themoviedb.org/3/tv/20169/credits?api_key=27466545a65659fcc5cb0e1571b23956
+
+// REVIEWS
+// https://api.themoviedb.org/3/tv/20169/reviews?api_key=27466545a65659fcc5cb0e1571b23956
+
+
+
+
+// ON THE AIR
+// https://api.themoviedb.org/3/tv/on_the_air?api_key=(API_KEY)
+
+// POPULAR
+// https://api.themoviedb.org/3/tv/popular?api_key=(API_KEY)
+
+// TOP RATED
+// https://api.themoviedb.org/3/tv/top_rated?api_key=(API_KEY)
+
+// TV SEARCH
+// https://api.themoviedb.org/3/search/tv?api_key=(API_KEY)&query=H
