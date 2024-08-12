241345630ea0d4b0b3c70b4c1b594bc7d27f9e40
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2024-08-12 22:00:28 +0530

    Updated API Calls on Dropdown In Game Section

--- Frontend/src/Components/Games/Games.jsx
+++ Frontend/src/Components/Games/Games.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card, Col, Dropdown, Form, Row } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import {
   // getGamesSectionData,
@@ -13,14 +13,20 @@ const Games = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [allgames, setAllGames] = useState([]);
+  const [platform, setPlatform] = useState("all");
+  const [category, setcategory] = useState("mmorpg");
+  const [sortBy, setSortBy] = useState("relevance");
 
   useEffect(() => {
     callAPI();
-  }, []);
+  }, [platform, category, sortBy]);
   const callAPI = async () => {
     const payload = {
-      category: "mmorpg",
+      category,
+      sortBy,
+      platform,
     };
+    // category="strategy",sortBy="release-date",platform="all"
     const response = await dispatch(getGamesSectionDataCategoryWise(payload));
     console.log(response);
     setAllGames(response);
@@ -28,112 +34,50 @@ const Games = () => {
   const particularGameCall = async (id) => {
     navigate(`/game/${id}`);
   };
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
+  const updatePlatform = (e) => {
+    setPlatform(e.target.value);
+  };
+  const updateCategory = (e) => {
+    setcategory(e.target.value);
+  };
+  const updateSortBy = (e) => {
+    setSortBy(e.target.value);
   };
   return (
     <>
       <div className=" my-2 ">
-        {/* <div>
-          <div className="relative inline-block text-left">
-            <div>
-              <button
-                type="button"
-                className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                onClick={toggleDropdown}
-              >
-                Options
-                <svg
-                  className="-mr-1 ml-2 h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
-
-            {isOpen && (
-              <div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="py-1">
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Option 1
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Option 2
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                  >
-                    Option 3
-                  </a>
-                </div>
-              </div>
-            )}
-          </div>
-        </div> */}
-        {/* <Dropdown>
-          <Dropdown.Toggle id="dropdown-basic">
-            All Platforms
-          </Dropdown.Toggle>
-
-          <Dropdown.Menu>
-            <Dropdown.Item href="#/action-1">Windows (PC)</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Browser (WEB)</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">All Platforms</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown> */}
-        {/* <Form.Select aria-label="Default select example" className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-          <option>All Platforms</option>
-          <option value="1">Windows (PC)</option>
-          <option value="2">Browser (WEB)</option>
-        </Form.Select> */}
-
-        <div className="flex justify-evenly">
-          <form class="">
+        <div className="flex justify-evenly flex-wrap">
+          <form className="">
             <label
-              for="default"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="default"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Platform
             </label>
             <select
               id="default"
-              class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              onChange={updatePlatform}
+              className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option selected>All Platforms</option>
-              <option value="CA">Windows (PC)</option>
-              <option value="FR">Browser (WEB)</option>
+              <option value="all">All Platforms</option>
+              <option value="pc">Windows (PC)</option>
+              <option value="browser">Browser (WEB)</option>
             </select>
           </form>
-          <form class="">
+          <form className="">
             <label
-              for="default"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="default"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
-              Platform
+              Category
             </label>
             <select
               id="default"
-              class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              onChange={updateCategory}
+              className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option selected value="mmo">MMO</option>
               <option value="mmorpg">MMORPG</option>
+              <option value="mmo">MMO</option>
               <option value="shooter">Shooter</option>
               <option value="strategy">Strategy</option>
               <option value="moba">Moba</option>
@@ -173,23 +117,28 @@ const Games = () => {
               <option value="permadeath">Permadeath</option>
             </select>
           </form>
-          <form class="">
+          <form className="">
             <label
-              for="default"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              htmlFor="default"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               Platform
             </label>
             <select
               id="default"
-              class="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              onChange={updateSortBy}
+              className="bg-gray-50 border border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option selected> Relevance</option>
-              <option value="CA">Popularity</option>
-              <option value="FR">Release Date</option>
-              <option value="FR">Alphabetical</option>
+              <option value="relevance"> Relevance</option>
+              <option value="popularity">Popularity</option>
+              <option value="release-date">Release Date</option>
+              <option value="alphabetical">Alphabetical</option>
             </select>
           </form>
+        </div>
+
+        <div className="font-semibold text-2xl text-gray-400 text-center">
+          Total ({allgames?.length})
         </div>
 
         <div className="flex flex-wrap justify-center pb-4 pt-10">
