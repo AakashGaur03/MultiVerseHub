d1084b3b011a8c9d6174dcee23aa343d5cc2b6bf
Aakash <aakash.sunrise23@gmail.com>
2024-08-20 16:41:40 +0530

    Update getImageFunc.js

--- Frontend/src/GlobalComp/getImageFunc.js
+++ Frontend/src/GlobalComp/getImageFunc.js
@@ -5,13 +5,17 @@ import { useDispatch } from "react-redux";
 export const getImageUrl = async (imageId, imageUrls, setImageUrls, setLoadingImages, dispatch) => {
   if (!imageUrls[imageId]) {
     try {
+      console.log("Enter The Code")
       const imageDB = await dispatch(getCricketImageDB(imageId));
+      
       if (imageDB) {
+        console.log("Enter The Code With Image",imageDB)
         setImageUrls((prevState) => ({
           ...prevState,
           [imageId]: imageDB.secureUrl,
         }));
       } else {
+      console.log("Enter The Code With out image")
         const response = await dispatch(getCricketImageCBs(imageId));
         if (response) {
           setImageUrls((prevState) => ({
@@ -23,9 +27,12 @@ export const getImageUrl = async (imageId, imageUrls, setImageUrls, setLoadingIm
           );
         }
       }
+      console.log("Exit The Code")
+
     } catch (error) {
       console.error("Error fetching image URL:", error);
     } finally {
+      console.log("Full Exit The Code")
       setLoadingImages((prevState) => ({
         ...prevState,
         [imageId]: false,
