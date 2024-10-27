d68962157f015d50908d6b44372f407ccc4b01f3
Aakash Gaur <aakash.sunrise23@gmail.com>
2024-10-27 22:31:36 +0530

    Like Button shown only user logged In

--- Frontend/src/GlobalComp/LikeButton.jsx
+++ Frontend/src/GlobalComp/LikeButton.jsx
@@ -1,13 +1,19 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
 import { Tooltip } from "react-tooltip";
 
 const LikeButton = ({ customId, customHeight = "50px", customWidth = "50px", isActive, onClick }) => {
   const buttonRef = useRef(null);
+  const isLoggedIn = useSelector((state) => state.getCurrentStatus.isUserLoggedIn);
+
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   const handleClick = () => {
     if (onClick) onClick();
   };
-
+  useEffect(() => {
+    setIsUserLoggedIn(isLoggedIn);
+  }, [isLoggedIn]);
   useEffect(() => {
     if (buttonRef.current) {
       buttonRef.current.dispatchEvent(new Event("mouseenter"));
@@ -16,30 +22,32 @@ const LikeButton = ({ customId, customHeight = "50px", customWidth = "50px", isA
   }, [isActive]);
 
   return (
-    <div>
-      <Tooltip
-        className="z-20"
-        place="left"
-        anchorSelect={`#${customId}`}
-        content={isActive ? "Remove From Favorite" : "Add To Favorite"}
-      />
-      <div ref={buttonRef} className="flex justify-center items-center" id={customId}>
-        <svg
-          onClick={handleClick}
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill={isActive ? "red" : "transparent"}
-          stroke="red"
-          strokeWidth="0.5"
-          className={`${
-            isActive ? "fill-red-500 animate-like" : "fill-transparent"
-          } transition-all duration-500 transform hover:scale-110 cursor-pointer`}
-          style={{ width: customWidth, height: customHeight }}
-        >
-          <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-        </svg>
+    isUserLoggedIn && (
+      <div>
+        <Tooltip
+          className="z-20"
+          place="left"
+          anchorSelect={`#${customId}`}
+          content={isActive ? "Remove From Favorite" : "Add To Favorite"}
+        />
+        <div ref={buttonRef} className="flex justify-center items-center" id={customId}>
+          <svg
+            onClick={handleClick}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill={isActive ? "red" : "transparent"}
+            stroke="red"
+            strokeWidth="0.5"
+            className={`${
+              isActive ? "fill-red-500 animate-like" : "fill-transparent"
+            } transition-all duration-500 transform hover:scale-110 cursor-pointer`}
+            style={{ width: customWidth, height: customHeight }}
+          >
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+          </svg>
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
