f88a28b0ef16f571420e232ae317cf2025528362
Aakash Gaur <aakash.sunrise23@gmail.com>
2024-11-24 22:12:18 +0530

    Update URL.js

--- Frontend/src/Api/URL.js
+++ Frontend/src/Api/URL.js
@@ -1,5 +1,4 @@
+// const API_URL = "http://localhost:8001"
+const API_URL = "https://multiversehub-backend.onrender.com";
 
-const API_URL = "http://localhost:8001"
-// const API_URL = "https://multiversehub-backend.onrender.com"
-
-export { API_URL }
+export { API_URL };
