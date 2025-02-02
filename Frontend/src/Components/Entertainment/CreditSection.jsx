d5d67e00263932deda30bbfcf35a083f0c5c04ab
Aakash Gaur <aakash.sunrise23@gmail.com>
2025-02-02 08:11:22 +0530

    Update

--- Frontend/src/Components/Entertainment/CreditSection.jsx
+++ Frontend/src/Components/Entertainment/CreditSection.jsx
@@ -5,18 +5,13 @@ import ImageWithLoader from "../../GlobalComp/ImageWithLoader";
 const CreditSection = ({ creditsData }) => {
   return (
     <div className="mt-5">
-      <div className="mb-2 mt-3 px-5 uppercase font-semibold text-2xl text-gray-300 text-center">
-        Cast
-      </div>
+      <div className="mb-2 mt-3 px-5 uppercase font-semibold text-2xl text-gray-300 text-center">Cast</div>
       <div className="overflow-y-auto flex my-2 px-5">
         <div className="flex gap-8 pb-4 pt-10">
           {creditsData?.cast?.length > 0 ? (
             creditsData.cast.map((data, index) => (
               <div className="activeClass relative" key={data.id}>
-                <Card
-                  style={{ width: "15rem" }}
-                  className="overflow-x-auto rounded-3xl "
-                >
+                <Card style={{ width: "15rem" }} className="overflow-x-auto rounded-3xl ">
                   {/* {data.profile_path ? (
                     <Card.Img
                       variant="top"
