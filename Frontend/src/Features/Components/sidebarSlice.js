0a90d457077e021965e4d0bb2a745bd2f2003a34
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-03-06 02:23:54 +0530

    Update

--- Frontend/src/Features/Components/sidebarSlice.js
+++ Frontend/src/Features/Components/sidebarSlice.js
@@ -1,23 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentSidebar: "",
-    toggleClicked: false,
+	currentSidebar: "",
+	toggleClicked: false,
 };
 
 const sidebarSlice = createSlice({
-    name: "sidebar",
-    initialState,
-    reducers: {
-        updateSidebar(state, action) {
-            state.currentSidebar = action.payload;
-        },
-        toggleClicked(state, action) {
-            state.toggleClicked = action.payload;
-        }
-    }
-})
+	name: "sidebar",
+	initialState,
+	reducers: {
+		updateSidebar(state, action) {
+			state.currentSidebar = action.payload;
+		},
+		toggleClicked(state, action) {
+			state.toggleClicked = action.payload;
+		},
+	},
+});
 
-export const { updateSidebar, toggleClicked } = sidebarSlice.actions
+export const { updateSidebar, toggleClicked } = sidebarSlice.actions;
 
 export default sidebarSlice.reducer;
