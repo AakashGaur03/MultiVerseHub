bcd8cff362906903bfcd0b12ac7fbfdb0e014815
Aakash Gaur <140057004+AakashGaur03@users.noreply.github.com>
2025-06-14 17:29:17 +0530

    Points Table and Ranking Light Theme Table Updates

--- Frontend/src/Components/Cricket/PointsTable.jsx
+++ Frontend/src/Components/Cricket/PointsTable.jsx
@@ -1,86 +1,88 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Table } from "react-bootstrap";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { getCricketPointsTable } from "../../Features";
 import { useDispatch, useSelector } from "react-redux";
 
 const PointsTable = () => {
-  const dispatch = useDispatch();
-  const pointsTableData = useSelector((state) => state.cricket.pointsTableData);
-  const loaderTrue = useSelector((state) => state.cricket.pointsTableStatus === "loading");
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(loaderTrue);
-  }, [loaderTrue]);
-  const [dataPoints, setDataPoints] = useState([]);
-  const { seriesId } = useParams();
+	const dispatch = useDispatch();
+	const pointsTableData = useSelector((state) => state.cricket.pointsTableData);
+	const loaderTrue = useSelector((state) => state.cricket.pointsTableStatus === "loading");
+	const [isLoading, setIsLoading] = useState(false);
+	const theme = useSelector((state) => state.theme.theme);
 
-  useEffect(() => {
-    const dataToStore = pointsTableData?.pointsTable || [];
-    setDataPoints(dataToStore);
-  }, [pointsTableData]);
+	useEffect(() => {
+		setIsLoading(loaderTrue);
+	}, [loaderTrue]);
+	const [dataPoints, setDataPoints] = useState([]);
+	const { seriesId } = useParams();
 
-  const callPointsTable = async () => {
-    try {
-      await dispatch(getCricketPointsTable(seriesId));
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	useEffect(() => {
+		const dataToStore = pointsTableData?.pointsTable || [];
+		setDataPoints(dataToStore);
+	}, [pointsTableData]);
 
-  useEffect(() => {
-    callPointsTable();
-  }, [seriesId]);
+	const callPointsTable = async () => {
+		try {
+			await dispatch(getCricketPointsTable(seriesId));
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  // console.log(seriesId);
-  return (
-    <>
-      {dataPoints.length > 0 ? (
-        <div className="w-full pl-0 md:pl-11">
-          {dataPoints.map((group, index) => (
-            <div key={index} className="mb-4">
-              <h4>{group.groupName}</h4>
-              <Table borderless hover variant="dark">
-                <thead>
-                  <tr>
-                    <th>Teams</th>
-                    <th>Mat</th>
-                    <th>Won</th>
-                    <th>Lost</th>
-                    <th>NR</th>
-                    <th>Pts</th>
-                    <th>NRR</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {group.pointsTableInfo.map((data, idx) => (
-                    <tr key={idx} className="d-fle x">
-                      <td>
-                        {data.teamName}
-                        {data.teamQualifyStatus && ` (${data.teamQualifyStatus})`}
-                      </td>
-                      <td>{data.matchesPlayed || 0}</td>
-                      <td>{data.matchesWon || 0}</td>
-                      <td>{data.matchesLost || 0}</td>
-                      <td>{data.noRes || 0}</td>
-                      <td>{data.points || 0}</td>
-                      <td>{data.nrr || 0}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </div>
-          ))}
-        </div>
-      ) : isLoading ? (
-        <div className="w-full flex justify-center">
-          <div className="loader"></div>
-        </div>
-      ) : (
-        <div>No Data to Show</div>
-      )}
-    </>
-  );
+	useEffect(() => {
+		callPointsTable();
+	}, [seriesId]);
+
+	// console.log(seriesId);
+	return (
+		<>
+			{dataPoints.length > 0 ? (
+				<div className="w-full pl-0 md:pl-11">
+					{dataPoints.map((group, index) => (
+						<div key={index} className="mb-4">
+							<h4>{group.groupName}</h4>
+							<Table borderless hover className={theme === "dark" ? "table-theme-dark" : "table-theme-light"}>
+								<thead>
+									<tr>
+										<th>Teams</th>
+										<th>Mat</th>
+										<th>Won</th>
+										<th>Lost</th>
+										<th>NR</th>
+										<th>Pts</th>
+										<th>NRR</th>
+									</tr>
+								</thead>
+								<tbody>
+									{group.pointsTableInfo.map((data, idx) => (
+										<tr key={idx} className="d-fle x">
+											<td>
+												{data.teamName}
+												{data.teamQualifyStatus && ` (${data.teamQualifyStatus})`}
+											</td>
+											<td>{data.matchesPlayed || 0}</td>
+											<td>{data.matchesWon || 0}</td>
+											<td>{data.matchesLost || 0}</td>
+											<td>{data.noRes || 0}</td>
+											<td>{data.points || 0}</td>
+											<td>{data.nrr || 0}</td>
+										</tr>
+									))}
+								</tbody>
+							</Table>
+						</div>
+					))}
+				</div>
+			) : isLoading ? (
+				<div className="w-full flex justify-center">
+					<div className="loader"></div>
+				</div>
+			) : (
+				<div className={theme === "dark" ? "text-gray-400" : "text-gray-700"}>No Data to Show</div>
+			)}
+		</>
+	);
 };
 
 export default PointsTable;
--- Frontend/src/Components/Cricket/Ranking.jsx
+++ Frontend/src/Components/Cricket/Ranking.jsx
@@ -11,236 +11,238 @@ import Badge from "react-bootstrap/Badge";
 import { getImageUrl } from "../../GlobalComp/getImageFunc";
 
 const Ranking = () => {
-  const storeRankingData = useSelector((state) => state.cricket.rankingData);
-  const loaderTrue = useSelector((state) => state.cricket.rankingStatus === "loading");
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(loaderTrue);
-  }, [loaderTrue]);
-  // const location = useLocation();
-  // const Data = location.state?.rankingsData;
-  const dispatch = useDispatch();
-  const [rankingData, setRankingData] = useState([]);
-  const [rankindDataType, setRankindDataType] = useState("");
-  const [imageUrls, setImageUrls] = useState({});
-  const [loadingImages, setLoadingImages] = useState({});
-  const [selectedGender, setSelectedGender] = useState("men");
-  const [selectedFormat, setSelectedFormat] = useState("test");
-  const [selectedCategory, setSelectedCategory] = useState("batsmen");
-  const [cache, setCache] = useState({});
-  const navigate = useNavigate();
+	const storeRankingData = useSelector((state) => state.cricket.rankingData);
+	const loaderTrue = useSelector((state) => state.cricket.rankingStatus === "loading");
+	const [isLoading, setIsLoading] = useState(false);
+	useEffect(() => {
+		setIsLoading(loaderTrue);
+	}, [loaderTrue]);
+	// const location = useLocation();
+	// const Data = location.state?.rankingsData;
+	const dispatch = useDispatch();
+	const theme = useSelector((state) => state.theme.theme);
 
-  // useEffect(() => {
-  //   if (Data) {
-  //     setRankingData(Data);
-  //     fetchImages(Data.rank);
-  //   }
-  // }, [Data]);
-  useEffect(() => {
-    // if (selectedGender === "women" && selectedFormat === "test") {
-    //   setSelectedFormat("odi");
-    // } else {
-    //   const cachedData = getCachedData();
-    //   if (cachedData) {
-    //     setRankingData(cachedData);
-    //     fetchImages(cachedData.rank);
-    //   } else {
-    updateSelectedFormat(selectedFormat);
-    callRankingApi();
-    // }
-    // }
-  }, [selectedGender, selectedFormat, selectedCategory]);
+	const [rankingData, setRankingData] = useState([]);
+	const [rankindDataType, setRankindDataType] = useState("");
+	const [imageUrls, setImageUrls] = useState({});
+	const [loadingImages, setLoadingImages] = useState({});
+	const [selectedGender, setSelectedGender] = useState("men");
+	const [selectedFormat, setSelectedFormat] = useState("test");
+	const [selectedCategory, setSelectedCategory] = useState("batsmen");
+	const [cache, setCache] = useState({});
+	const navigate = useNavigate();
 
-  // const getCachedData = () => {
-  //   const key = `${selectedGender}-${selectedFormat}-${selectedCategory}`;
-  //   return cache[key];
-  // };
-  const updateCache = (data) => {
-    const key = `${selectedGender}-${selectedFormat}-${selectedCategory}`;
-    setCache((prevCache) => ({ ...prevCache, [key]: data }));
-  };
+	// useEffect(() => {
+	//   if (Data) {
+	//     setRankingData(Data);
+	//     fetchImages(Data.rank);
+	//   }
+	// }, [Data]);
+	useEffect(() => {
+		// if (selectedGender === "women" && selectedFormat === "test") {
+		//   setSelectedFormat("odi");
+		// } else {
+		//   const cachedData = getCachedData();
+		//   if (cachedData) {
+		//     setRankingData(cachedData);
+		//     fetchImages(cachedData.rank);
+		//   } else {
+		updateSelectedFormat(selectedFormat);
+		callRankingApi();
+		// }
+		// }
+	}, [selectedGender, selectedFormat, selectedCategory]);
 
-  const fetchImages = async (rankings) => {
-    const newLoadingImages = {};
-    rankings?.forEach((data) => {
-      const imageId = data.faceImageId || data.imageId;
-      newLoadingImages[imageId] = true;
-    });
-    setLoadingImages(newLoadingImages);
+	// const getCachedData = () => {
+	//   const key = `${selectedGender}-${selectedFormat}-${selectedCategory}`;
+	//   return cache[key];
+	// };
+	const updateCache = (data) => {
+		const key = `${selectedGender}-${selectedFormat}-${selectedCategory}`;
+		setCache((prevCache) => ({ ...prevCache, [key]: data }));
+	};
 
-    if (rankings) {
-      for (const data of rankings) {
-        const imageId = data.faceImageId || data.imageId;
-        // await getImageUrl(imageId);
-        await getImageUrl(imageId, imageUrls, setImageUrls, setLoadingImages, dispatch);
-        // await new Promise((resolve) => setTimeout(resolve, 500)); // Adding 500ms delay between each fetch
-      }
-    }
-  };
-  const updateSelectedFormat = (format) => {
-    if (selectedGender === "women" && format === "test") {
-      setSelectedFormat("odi");
-    } else {
-      setSelectedFormat(format);
-    }
-  };
+	const fetchImages = async (rankings) => {
+		const newLoadingImages = {};
+		rankings?.forEach((data) => {
+			const imageId = data.faceImageId || data.imageId;
+			newLoadingImages[imageId] = true;
+		});
+		setLoadingImages(newLoadingImages);
 
-  const callRankingApi = async () => {
-    try {
-      let format = selectedFormat;
-      let category = selectedCategory;
-      let isWomen = selectedGender === "women" ? "1" : "";
-      let payload = {
-        format: selectedFormat,
-        category: selectedCategory,
-        isWomen: selectedGender === "women" ? "1" : "",
-        prevData: storeRankingData,
-      };
-      let name = `${format}${isWomen}${category}`;
-      if (!(selectedFormat == "test" && selectedGender == "women")) {
-        setRankindDataType(name);
-        await dispatch(getCricketRanking(payload));
-      }
-    } catch (error) {
-      console.error("Error fetching ranking data:", error);
-    }
-  };
+		if (rankings) {
+			for (const data of rankings) {
+				const imageId = data.faceImageId || data.imageId;
+				// await getImageUrl(imageId);
+				await getImageUrl(imageId, imageUrls, setImageUrls, setLoadingImages, dispatch);
+				// await new Promise((resolve) => setTimeout(resolve, 500)); // Adding 500ms delay between each fetch
+			}
+		}
+	};
+	const updateSelectedFormat = (format) => {
+		if (selectedGender === "women" && format === "test") {
+			setSelectedFormat("odi");
+		} else {
+			setSelectedFormat(format);
+		}
+	};
 
-  useEffect(() => {
-    setRankingData(storeRankingData?.[rankindDataType]);
-    updateCache(storeRankingData?.[rankindDataType]);
-    fetchImages(storeRankingData?.[rankindDataType]?.rank);
-  }, [storeRankingData]);
+	const callRankingApi = async () => {
+		try {
+			let format = selectedFormat;
+			let category = selectedCategory;
+			let isWomen = selectedGender === "women" ? "1" : "";
+			let payload = {
+				format: selectedFormat,
+				category: selectedCategory,
+				isWomen: selectedGender === "women" ? "1" : "",
+				prevData: storeRankingData,
+			};
+			let name = `${format}${isWomen}${category}`;
+			if (!(selectedFormat == "test" && selectedGender == "women")) {
+				setRankindDataType(name);
+				await dispatch(getCricketRanking(payload));
+			}
+		} catch (error) {
+			console.error("Error fetching ranking data:", error);
+		}
+	};
 
-  const callParticularPlayer = (playerId) => {
-    // console.log("wdsddssdsd");
-    if (selectedCategory != "teams") {
-      navigate(`/cricket/playerInfo/${playerId}`);
-      window.scrollTo(0, 0);
-    }
-  };
+	useEffect(() => {
+		setRankingData(storeRankingData?.[rankindDataType]);
+		updateCache(storeRankingData?.[rankindDataType]);
+		fetchImages(storeRankingData?.[rankindDataType]?.rank);
+	}, [storeRankingData]);
 
-  return (
-    <div className="w-full pl-0 md:pl-11">
-      <div className="mt-4">
-        <Badge
-          pill
-          className={`fs-6 me-5 cursor-pointer ${selectedGender === "men" ? "bg-primary" : "bg-secondary"}`}
-          onClick={() => setSelectedGender("men")}
-        >
-          Men
-        </Badge>
-        <Badge
-          pill
-          className={`fs-6 me-5 cursor-pointer ${selectedGender === "women" ? "bg-primary" : "bg-secondary"}`}
-          onClick={() => setSelectedGender("women")}
-        >
-          Women
-        </Badge>
-      </div>
-      <div className="mt-4 mb-3">
-        {selectedGender == "men" && (
-          <Badge
-            pill
-            className={`fs-6 me-3 cursor-pointer ${selectedFormat === "test" ? "bg-primary" : "bg-secondary"} `}
-            onClick={() => updateSelectedFormat("test")}
-          >
-            Test
-          </Badge>
-        )}
-        <Badge
-          pill
-          className={`fs-6 me-3 cursor-pointer ${selectedFormat === "odi" ? "bg-primary" : "bg-secondary"} `}
-          onClick={() => updateSelectedFormat("odi")}
-        >
-          ODI
-        </Badge>
-        <Badge
-          pill
-          className={`fs-6 me-3 cursor-pointer ${selectedFormat === "t20" ? "bg-primary" : "bg-secondary"} `}
-          onClick={() => updateSelectedFormat("t20")}
-        >
-          T20
-        </Badge>
-      </div>
-      <div>
-        <Badge
-          pill
-          className={`fs-6 me-5 cursor-pointer ${selectedCategory === "batsmen" ? "bg-primary" : "bg-secondary"}`}
-          onClick={() => setSelectedCategory("batsmen")}
-        >
-          Batting
-        </Badge>
-        <Badge
-          pill
-          className={`fs-6 me-5 cursor-pointer ${selectedCategory === "bowlers" ? "bg-primary" : "bg-secondary"}`}
-          onClick={() => setSelectedCategory("bowlers")}
-        >
-          Bowling
-        </Badge>
-        <Badge
-          pill
-          className={`fs-6 me-5 cursor-pointer ${selectedCategory === "allrounders" ? "bg-primary" : "bg-secondary"}`}
-          onClick={() => setSelectedCategory("allrounders")}
-        >
-          All-rounders
-        </Badge>
-        <Badge
-          pill
-          className={`fs-6 me-5 cursor-pointer ${selectedCategory === "teams" ? "bg-primary" : "bg-secondary"}`}
-          onClick={() => setSelectedCategory("teams")}
-        >
-          Teams
-        </Badge>
-      </div>
-      {isLoading ? (
-        <div className="w-full flex justify-center mt-5">
-          <div className="loader"></div>
-        </div>
-      ) : rankingData?.rank?.length > 0 ? (
-        <Table responsive className="table" borderless hover variant="dark">
-          <thead>
-            <tr>
-              <th>Position</th>
-              {selectedCategory !== "teams" && <th>Player</th>}
-              {selectedCategory == "teams" && <th>Teams</th>}
-              <th>Info</th>
-              {selectedCategory == "teams" && <th>Ratings</th>}
-              <th>Points</th>
-            </tr>
-          </thead>
-          <tbody>
-            {rankingData.rank.slice(0, 15).map((data, index) => {
-              const imageId = data.faceImageId || data.imageId;
-              return (
-                <tr key={index} className="d-fle x" onClick={() => callParticularPlayer(data.id)}>
-                  <td>{data.rank || "-"}</td>
-                  {loadingImages[imageId] ? (
-                    <td>Loading...</td>
-                  ) : (
-                    <td>
-                      <img style={{ maxHeight: "68px", maxWidth: "90px" }} src={imageUrls[imageId]} alt="" />
-                    </td>
-                  )}
-                  <td>
-                    <>
-                      {data.name || "-"}
-                      {selectedCategory == "teams" && data.matches && <div>Matches: {data.matches || "-"}</div>}
-                      {selectedCategory !== "teams" && <div>{data.country || "-"}</div>}
-                    </>
-                  </td>
-                  {selectedCategory == "teams" && <td>{data.rating || "-"}</td>}
-                  <td>{data.points || "-"}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      ) : (
-        <div>No Data Available</div>
-      )}
-    </div>
-  );
+	const callParticularPlayer = (playerId) => {
+		// console.log("wdsddssdsd");
+		if (selectedCategory != "teams") {
+			navigate(`/cricket/playerInfo/${playerId}`);
+			window.scrollTo(0, 0);
+		}
+	};
+
+	return (
+		<div className="w-full pl-0 md:pl-11">
+			<div className="mt-4">
+				<Badge
+					pill
+					className={`fs-6 me-5 cursor-pointer ${selectedGender === "men" ? "bg-primary" : "bg-secondary"}`}
+					onClick={() => setSelectedGender("men")}
+				>
+					Men
+				</Badge>
+				<Badge
+					pill
+					className={`fs-6 me-5 cursor-pointer ${selectedGender === "women" ? "bg-primary" : "bg-secondary"}`}
+					onClick={() => setSelectedGender("women")}
+				>
+					Women
+				</Badge>
+			</div>
+			<div className="mt-4 mb-3">
+				{selectedGender == "men" && (
+					<Badge
+						pill
+						className={`fs-6 me-3 cursor-pointer ${selectedFormat === "test" ? "bg-primary" : "bg-secondary"} `}
+						onClick={() => updateSelectedFormat("test")}
+					>
+						Test
+					</Badge>
+				)}
+				<Badge
+					pill
+					className={`fs-6 me-3 cursor-pointer ${selectedFormat === "odi" ? "bg-primary" : "bg-secondary"} `}
+					onClick={() => updateSelectedFormat("odi")}
+				>
+					ODI
+				</Badge>
+				<Badge
+					pill
+					className={`fs-6 me-3 cursor-pointer ${selectedFormat === "t20" ? "bg-primary" : "bg-secondary"} `}
+					onClick={() => updateSelectedFormat("t20")}
+				>
+					T20
+				</Badge>
+			</div>
+			<div>
+				<Badge
+					pill
+					className={`fs-6 me-5 cursor-pointer ${selectedCategory === "batsmen" ? "bg-primary" : "bg-secondary"}`}
+					onClick={() => setSelectedCategory("batsmen")}
+				>
+					Batting
+				</Badge>
+				<Badge
+					pill
+					className={`fs-6 me-5 cursor-pointer ${selectedCategory === "bowlers" ? "bg-primary" : "bg-secondary"}`}
+					onClick={() => setSelectedCategory("bowlers")}
+				>
+					Bowling
+				</Badge>
+				<Badge
+					pill
+					className={`fs-6 me-5 cursor-pointer ${selectedCategory === "allrounders" ? "bg-primary" : "bg-secondary"}`}
+					onClick={() => setSelectedCategory("allrounders")}
+				>
+					All-rounders
+				</Badge>
+				<Badge
+					pill
+					className={`fs-6 me-5 cursor-pointer ${selectedCategory === "teams" ? "bg-primary" : "bg-secondary"}`}
+					onClick={() => setSelectedCategory("teams")}
+				>
+					Teams
+				</Badge>
+			</div>
+			{isLoading ? (
+				<div className="w-full flex justify-center mt-5">
+					<div className="loader"></div>
+				</div>
+			) : rankingData?.rank?.length > 0 ? (
+				<Table responsive className={theme === "dark" ? "table-theme-dark" : "table-theme-light"} borderless hover>
+					<thead>
+						<tr>
+							<th>Position</th>
+							{selectedCategory !== "teams" && <th>Player</th>}
+							{selectedCategory == "teams" && <th>Teams</th>}
+							<th>Info</th>
+							{selectedCategory == "teams" && <th>Ratings</th>}
+							<th>Points</th>
+						</tr>
+					</thead>
+					<tbody>
+						{rankingData.rank.slice(0, 15).map((data, index) => {
+							const imageId = data.faceImageId || data.imageId;
+							return (
+								<tr key={index} className="d-fle x" onClick={() => callParticularPlayer(data.id)}>
+									<td>{data.rank || "-"}</td>
+									{loadingImages[imageId] ? (
+										<td>Loading...</td>
+									) : (
+										<td>
+											<img style={{ maxHeight: "68px", maxWidth: "90px" }} src={imageUrls[imageId]} alt="" />
+										</td>
+									)}
+									<td>
+										<>
+											{data.name || "-"}
+											{selectedCategory == "teams" && data.matches && <div>Matches: {data.matches || "-"}</div>}
+											{selectedCategory !== "teams" && <div>{data.country || "-"}</div>}
+										</>
+									</td>
+									{selectedCategory == "teams" && <td>{data.rating || "-"}</td>}
+									<td>{data.points || "-"}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</Table>
+			) : (
+				<div>No Data Available</div>
+			)}
+		</div>
+	);
 };
 
 export default Ranking;
